60a68d9d00171d7b6b2f4f3ca813b284d7ed31cc
Kelly Steensma <kellysteensma@gmail.com>
2013-09-16 17:32:19 -0500

    Adding game loop.

--- src/Game.java
+++ src/Game.java
@@ -1,17 +1,16 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Game {
   public Io io;
-  public ArrayList<String> turnHistory;
   public static final Map<Enum, String> MESSAGES = createMessagesMap();
   public static final int TURN_LIMIT = 6;
+  public LinkedHashMap<String, Boolean> questions;
+  public LinkedHashMap<String, Boolean> turnHistory;
 
   public Game(Io io) {
     this.io = io;
-    turnHistory = new ArrayList<String>();
+    questions = new QuestionsDatabase().questions;
+    turnHistory = new LinkedHashMap<String, Boolean>();
   }
 
   public void start() {
@@ -21,38 +20,75 @@ public class Game {
   public void play() {
     while (turnHistory.size() < TURN_LIMIT) {
       displayQuestion();
-      String response = getUserResponse();
-      storeResponse(response);
-  //    deliver feedback
+      boolean response = getUserResponse();
+      boolean correctness = validateUserResponse(response);
+      storeResponse(getQuestion(), correctness);
     }
+    displayGameResult();
+  }
+
+  private void displayGameResult() {
+    Enum game_result = MessageText.GAME_RESULT;
+    String gameResultText = MESSAGES.get(game_result);
+    io.out(gameResultText + summarizeTurnHistory());
+  }
+
+  private boolean validateUserResponse(boolean response) {
+    boolean answer = getAnswer();
+    return answer == response;
+  }
+
+  private int getQuestionNumber() {
+    return turnHistory.size();
+  }
+
+  private String getQuestion() {
+    int questionNumber = getQuestionNumber();
+    ArrayList<String> questionList = new ArrayList<String>(questions.keySet());
+    return questionList.get(questionNumber);
+  }
+
+  private boolean getAnswer() {
+    int questionNumber = getQuestionNumber();
+    ArrayList<Boolean> answerList = new ArrayList(questions.values());
+    return answerList.get(questionNumber);
   }
 
   public void displayWelcomeMessage() {
-    Enum welcome = MessageType.WELCOME;
+    Enum welcome = MessageText.WELCOME;
     io.out(MESSAGES.get(welcome));
   }
 
-  public void displayQuestion() {
-    Enum askQuestion = MessageType.ASK_QUESTION;
-    io.out(MESSAGES.get(askQuestion));
+  private void displayQuestion() {
+    io.out(getQuestion());
   }
 
-  private String getUserResponse() {
-    return io.in();
+  private boolean getUserResponse() {
+    String response = io.in();
+    return Boolean.parseBoolean(response);
   }
 
-  private void storeResponse(String response) {
-    turnHistory.add(response);
+  private void storeResponse(String question, Boolean correctness) {
+    turnHistory.put(question, correctness);
   }
 
-  public static enum MessageType {
-    WELCOME, ASK_QUESTION
+  public int summarizeTurnHistory() {
+    int trueCount = 0;
+    ArrayList<Boolean> correctnessList = new ArrayList<Boolean>(turnHistory.values());
+    for(int i=0; i< turnHistory.size(); i++) {
+       trueCount += correctnessList.get(i) == true ? 1 : 0;
+    }
+    return trueCount;
+  }
+
+  public static enum MessageText {
+    WELCOME, GAME_RESULT
   }
 
   public static Map<Enum, String> createMessagesMap() {
       Map<Enum, String> result = new HashMap<Enum, String>();
-      result.put(MessageType.WELCOME, "Welcome to the game!");
-      result.put(MessageType.ASK_QUESTION, "Is this tweet true or false?");
+      result.put(MessageText.WELCOME, "Welcome to the game!");
+      result.put(MessageText.GAME_RESULT, "Here is your final score: ");
       return Collections.unmodifiableMap(result);
   }
 }
--- test/GameTest.java
+++ test/GameTest.java
@@ -2,6 +2,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -10,35 +11,61 @@ public class GameTest {
   Game game;
   MockIo io;
   List expectedOutput;
+  ArrayList questionsList;
 
   @Before
   public void setUp() {
     ArrayList input = new ArrayList();
-    for(int i=0; i<6; i++) {
-      input.add("true");
-    }
-    expectedOutput = new ArrayList();
+    input.add("true");
+    input.add("true");
+    input.add("true");
+    input.add("true");
+    input.add("true");
+    input.add("false");
     io = new MockIo(input);
     game = new Game(io);
+    LinkedHashMap questions = new QuestionsDatabase().questions;
+    questionsList = new ArrayList(questions.keySet());
   }
 
   @Test
   public void start() {
     game.start();
-    expectedOutput.add(game.MESSAGES.get(Game.MessageType.WELCOME));
+    expectedOutput = new ArrayList();
+    expectedOutput.add(game.MESSAGES.get(Game.MessageText.WELCOME));
     assertEquals(expectedOutput, io.getOutput());
   }
 
   @Test
   public void play() {
     game.play();
-    ArrayList expectedTurnHistory = new ArrayList();
-    for(int i=0; i<6; i++) {
-      expectedTurnHistory.add("true");
-      expectedOutput.add(game.MESSAGES.get(Game.MessageType.ASK_QUESTION));
-    }
-
-    assertEquals(expectedOutput, io.getOutput());
+    LinkedHashMap expectedTurnHistory = new LinkedHashMap<String, Boolean>();
+    expectedTurnHistory.put(questionsList.get(0), true);
+    expectedTurnHistory.put(questionsList.get(1), true);
+    expectedTurnHistory.put(questionsList.get(2), true);
+    expectedTurnHistory.put(questionsList.get(3), true);
+    expectedTurnHistory.put(questionsList.get(4), true);
+    expectedTurnHistory.put(questionsList.get(5), false);
     assertEquals(expectedTurnHistory, game.turnHistory);
+
+    expectedOutput = new ArrayList();
+    expectedOutput.add(questionsList.get(0));
+    expectedOutput.add(questionsList.get(1));
+    expectedOutput.add(questionsList.get(2));
+    expectedOutput.add(questionsList.get(3));
+    expectedOutput.add(questionsList.get(4));
+    expectedOutput.add(questionsList.get(5));
+    expectedOutput.add(game.MESSAGES.get(Game.MessageText.GAME_RESULT) + 5);
+    assertEquals(expectedOutput, io.getOutput());
+  }
+
+  @Test
+  public void summarizeTurnHistory() {
+    LinkedHashMap expectedTurnHistory = new LinkedHashMap<String, Boolean>();
+    for(int i=0; i<6; i++) {
+      expectedTurnHistory.put("True or false " + i, true);
+    }
+    game.turnHistory = expectedTurnHistory;
+    assertEquals(6, game.summarizeTurnHistory());
   }
 }
